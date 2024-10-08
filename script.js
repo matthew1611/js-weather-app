b45c2e8db9ad3eb0a9f3af5a9bb391e5bba1cd7f
Matthew <69815486+matthew1611@users.noreply.github.com>
2024-10-08 17:10:34 -0500

    adds error message to invalid city name

--- script.js
+++ script.js
@@ -8,28 +8,36 @@ const weatherIcon = document.querySelector('.weather-icon');
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  var data = await response.json();
 
-  console.log(data);
-
-  document.querySelector('.city').innerHTML = data.name;
-  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
-  document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-  document.querySelector('.wind').innerHTML = data.wind.speed + ' m/h';
-
-  if (data.weather[0].main == 'Clouds') {
-    weatherIcon.src = 'images/clouds.png';
-  } else if (data.weather[0].main == 'Clear') {
-    weatherIcon.src = 'images/clear.png';
-  } else if (data.weather[0].main == 'Drizzle') {
-    weatherIcon.src = 'images/drizzle.png';
-  } else if (data.weather[0].main == 'Rain') {
-    weatherIcon.src = 'images/rain.png';
-  } else if (data.weather[0].main == 'Snow') {
-    weatherIcon.src = 'images/snow.png';
+  if (response.status == 404) {
+    document.querySelector('.error').style.display = 'block';
+    document.querySelector('.weather').style.display = 'none';
   } else {
-    data.weather[0].main == 'Clear';
-    weatherIcon.src = 'images/clear.png';
+    var data = await response.json();
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML =
+      Math.round(data.main.temp) + '°F';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' m/h';
+
+    if (data.weather[0].main == 'Clouds') {
+      weatherIcon.src = 'images/clouds.png';
+    } else if (data.weather[0].main == 'Clear') {
+      weatherIcon.src = 'images/clear.png';
+    } else if (data.weather[0].main == 'Drizzle') {
+      weatherIcon.src = 'images/drizzle.png';
+    } else if (data.weather[0].main == 'Rain') {
+      weatherIcon.src = 'images/rain.png';
+    } else if (data.weather[0].main == 'Snow') {
+      weatherIcon.src = 'images/snow.png';
+    } else {
+      data.weather[0].main == 'Clear';
+      weatherIcon.src = 'images/clear.png';
+    }
+
+    document.querySelector('.weather').style.display = 'block';
+    document.querySelector('.error').style.display = 'none';
   }
 }
 
